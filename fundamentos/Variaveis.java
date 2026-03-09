e6c54600e9f6496f7e254f551b3bcae32cc9233b
Isabella Portela <isaportela6@gmail.com>
2026-03-08 21:40:34 -0300

    feat: add first Java variables exercise

---
+++ fundamentos/Variaveis.java
@@ -0,0 +1,13 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        String nome = "Isabella";
+        int idade = 24;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+
+    }
+
+}
