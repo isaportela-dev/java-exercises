01b02a9df149654f276a4b3f83fb107ac6273dd2
Isabella Portela <isaportela6@gmail.com>
2026-03-10 22:38:39 -0300

    feat: add even or odd conditional exercise

---
+++ condicionais/EvenOrOdd.java
@@ -0,0 +1,22 @@
+package condicionais;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("The number is EVEN");
+        } else {
+            System.out.println("The number is ODD");
+        }
+
+        scanner.close();
+    }
+}
