2a64612f29c11358ea6f8fa9bc4d2d75c087c597
Isabella Portela <isaportela6@gmail.com>
2026-03-10 23:06:30 -0300

    feat: add multiplication table loop exercise

---
+++ loops/MultiplicationTable.java
@@ -0,0 +1,23 @@
+package loops;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("\nMultiplication Table:");
+
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + " x " + i + " = " + result);
+        }
+
+        scanner.close();
+    }
+}
