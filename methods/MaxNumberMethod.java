a9a13ce3281bec2de5896b3cee7b966d4f3424aa
Isabella Portela <isaportela6@gmail.com>
2026-03-11 22:06:10 -0300

    feat: add max number method exercise

---
+++ methods/MaxNumberMethod.java
@@ -0,0 +1,41 @@
+package methods;
+
+import java.util.Scanner;
+
+public class MaxNumberMethod {
+
+    public static int findMax(int a, int b, int c) {
+
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int n1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int n2 = scanner.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int n3 = scanner.nextInt();
+
+        int result = findMax(n1, n2, n3);
+
+        System.out.println("The largest number is: " + result);
+
+        scanner.close();
+    }
+}
